033918dd1a3c6961c518c211c835dcdc72dba5fb
pitt <dpittmor@gmail.com>
2025-05-07 16:03:38 +0100

    Adds question set to quiz
    Adds a comprehensive set of questions to the quiz, including hardware, software, networking,
    and history themes.

--- src/quiz_manager.py
+++ src/quiz_manager.py
@@ -11,7 +11,7 @@ class QuizManager:
         
     def loadQuestions(self, file_path):
         try:
-            with open(file_path, 'r') as file:
+            with open(file_path, 'r', encoding='utf-8') as file:
                 data = json.load(file)
                 all_questions = data.get('questions', [])
                 
